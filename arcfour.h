b2f6a7f8e08f427a0914aa4fcec62d8af2b14b9b
Ashadowmonarch <forcoding39@gmail.com>
2025-07-01 10:24:28 -0400

    define types

--- arcfour.h
+++ arcfour.h
@@ -1,1 +1,11 @@
 /* arcfour.h */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef unsigned char int8;
+typedef unsigned short int int16;
+typedef unsigned int int32;
+
--- example.c
+++ example.c
@@ -1,1 +1,13 @@
 /* example.c */
+
+#include "arcfour.h"
+
+int main (void);
+
+int main(){
+    char *key, *from, *to, *encrypted, *decrypted;
+
+    key = "tomatoes"; // change to RSA 2048 keygen later
+
+    from = "Shall I compare thee to a summer's day?";
+}
