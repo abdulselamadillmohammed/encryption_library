1c757c7209b12eb99f883a5bfab3ccbd10865784
Ashadowmonarch <forcoding39@gmail.com>
2025-07-02 01:59:23 -0400

    update

--- arcfour.h
+++ arcfour.h
@@ -5,7 +5,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct s_arcfour {
+    // ...
+};
+
 typedef unsigned char int8;
 typedef unsigned short int int16;
 typedef unsigned int int32;
 
+Arcfour *rc4init(int8*, int16);
+int8 *rc4encrypt(int8*, int16);
--- example.c
+++ example.c
@@ -2,12 +2,24 @@
 
 #include "arcfour.h"
 
+#define F fflush(stdout)
+
 int main (void);
 
 int main(){
+    Arcfour *rc4;
+    int16 skey, stext;
     char *key, *from, *to, *encrypted, *decrypted;
 
     key = "tomatoes"; // change to RSA 2048 keygen later
+    skey = strlen(key);
 
     from = "Shall I compare thee to a summer's day?";
+    stext = strlen(from);
+
+    printf("Intializing encryption..."); F;
+    rc4 = rc4init(key, skey); // check if it returns 0 in prod, cause of malloc checking
+    printf("done\n");
+
+    printf("'%s'\n ->", from);
 }
