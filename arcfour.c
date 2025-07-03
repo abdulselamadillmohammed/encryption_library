19bd82957ec3c25eacc05767c440d063c554f436
Ashadowmonarch <forcoding39@gmail.com>
2025-07-03 13:16:41 -0400

    create function to create an rc4 byte

--- arcfour.c
+++ arcfour.c
@@ -38,3 +38,35 @@ Arcfour *rc4init(int8 *key, int16 size){
     return p;
 
 };
+
+/*
+
+while GeneratingOutput:
+    i := i + 1
+    j1 := j1 + S1[i]
+    swap values of S1[i] and S1[j1]
+    output S2[S1[i] + S1[j1]]
+    j2 := j2 + S2[i]
+    swap values of S2[i] and S2[j2]
+    output S1[S2[i] + S2[j2]]
+endwhile
+
+*/
+
+int8 rc4byte(Arcfour *p){
+    int16 tmp1, tmp2;
+    
+    p->i = (p->i + 1) % 256;
+    p->j = (p->j + p->s[p->i]) % 256;
+
+    tmp1 = p->s[p->i];
+    tmp2 = p->s[p->j];
+    p->s[p->i] = tmp2;
+    p->s[p->j] = tmp1;
+
+
+    tmp1 = (p->s[p->i] + p->s[p->j]) % 256;
+    p->k = p->s[tmp1];  
+
+    return p->k;
+};
